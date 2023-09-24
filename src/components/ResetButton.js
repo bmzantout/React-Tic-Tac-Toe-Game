958c1b93b08ddb154f9a8708e44ca4b52ce06f43
Basme Zantout <105554803+bmzantout@users.noreply.github.com>
2023-09-24 16:42:08 +0300

    Delete src directory

--- src/components/ResetButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./ResetButton.css";
-
-export const ResetButton = ({resetBoard}) =>{
-
-    return(
-        <button className="reset-button" onClick={resetBoard}>Reset</button>
-    )
-}
